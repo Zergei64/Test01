58537f52f57c80fcfa12b6a0adfac987ef6d9296
Zergei64 <zsoftz@gmail.com>
2017-05-09 12:22:14 -0600

    Update test_spec.rb

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 require "test/unit"
 
+Test::Unit.run = true if defined?(Test::Unit) && Test::Unit.respond_to?(:run=)
+
 class Test01 < Test::Unit::TestCase
  
   def test_simple
