76d5ee3f6c667184a1d3295018f0a92937f3661b
Zergei64 <zsoftz@gmail.com>
2017-05-10 10:17:57 -0600

    Update test_spec.rb

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 RSpec.describe "something" do
   it "does something" do
-    expect(5).to eq 5
+    #expect(5).to eq 5
+    expect(SimpleNumber.new(2).add(2)).to eq 4
+    expect(SimpleNumber.new(2).multiply(3)).to eq 6
   end
 end
