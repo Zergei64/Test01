d99ede3df1a38a3e35887c30a501548658ddb458
Zergei64 <zsoftz@gmail.com>
2017-05-10 15:55:07 -0600

    Update test_test.rb

--- test/test_test.rb
+++ test/test_test.rb
@@ -8,5 +8,9 @@ class TestSimpleNumber < Test::Unit::TestCase
     assert_equal(4, SimpleNumber.new(2).add(2) )
     assert_equal(6, SimpleNumber.new(2).multiply(3) )
   end
+  
+ def test_simple_Error
+    assert_equal(5, SimpleNumber.new(2).add(2) )
+  end
  
 end
